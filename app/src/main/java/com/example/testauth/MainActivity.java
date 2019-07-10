837fddb5736d724d6645cfd55f17f45331345f39
ig <ig@pss.one>
2019-07-10 12:34:18 +0300

    add AuthUI add EmailAuth add GoogleAuth add PhoneAuth

--- app/src/main/java/com/example/testauth/MainActivity.java
+++ app/src/main/java/com/example/testauth/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.testauth;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,13 +12,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_REQUEST_CODE = 7117;
     EditText login;
     EditText password;
     Button signIn;
@@ -24,12 +33,15 @@ public class MainActivity extends AppCompatActivity {
 
     Button email;
     Button phone;
-    Button google;
+    Button authUI;
     Button fingerprint;
+    Button logout;
     TextView mode;
 
-    enum mode {Email,Phone,Google,Fingerprint};
     Boolean isModeEmail;
+
+
+    List<AuthUI.IdpConfig> providers;
 
 
     private FirebaseAuth mAuth;
@@ -39,9 +51,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //init provider
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+
+        );
+        //showSignInOptions();
 
         mAuth = FirebaseAuth.getInstance();
-
         login = findViewById(R.id.loginText);
         password = findViewById(R.id.passwordText);
         signIn = findViewById(R.id.buttonSignIn);
@@ -49,12 +68,35 @@ public class MainActivity extends AppCompatActivity {
 
         email = findViewById(R.id.buttonEmail);
         phone = findViewById(R.id.buttonPhone);
-        google = findViewById(R.id.buttonGoogle);
+        authUI = findViewById(R.id.buttonAuthUI);
         fingerprint = findViewById(R.id.buttonFingerprint);
         mode = findViewById(R.id.textViewMode);
 
 
+        logout =findViewById(R.id.buttonLogOut);
 
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //logout
+                AuthUI.getInstance()
+                        .signOut(MainActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                logout.setEnabled(false);
+                                showSignInOptions();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this," "+e.getMessage(),Toast.LENGTH_SHORT);
+
+                    }
+                });
+            }
+        });
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -66,8 +108,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-
-
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -77,7 +117,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -93,21 +132,26 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivities(new Intent(MainActivity.this, ActivityTwo.class));
+            }
+        });
+        authUI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSignInOptions();
+            }
+        });
+
 
     }
 
 
 
 
-
-
-
-
-
-
-
-
-        public void signIn(String login,String password ){
+    public void signIn(String login,String password ){
         if (isModeEmail){
             mAuth.signInWithEmailAndPassword(login,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                 @Override
@@ -140,4 +184,34 @@ public class MainActivity extends AppCompatActivity {
 
                     }
         }
+
+
+
+
+    private void showSignInOptions() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.MyTheme)
+                        .build(), MY_REQUEST_CODE
+        );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MY_REQUEST_CODE){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (requestCode == RESULT_OK){
+                //Get user
+                FirebaseUser user =FirebaseAuth.getInstance().getCurrentUser();
+                //Show email toast
+                Toast.makeText(this," "+user.getEmail(),Toast.LENGTH_SHORT);
+                logout.setEnabled(true);
+            }else {
+                Toast.makeText(this," "+response.getError(),Toast.LENGTH_SHORT);
+            }
+        }
+
+    }
 }
